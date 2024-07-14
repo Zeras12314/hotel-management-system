b3908fc654a688f29641a691e73b7e14e88ce3db
Gerson Tiongson <gerson.tiongson.12314@gmail.com>
2024-07-14 22:50:48 +0800

    responsive style

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  showSidebar = true;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
